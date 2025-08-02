7af71745328643b1cdc98ffa1b1e7ccbd74538e8
aanmmoool <anmolsinghtron123@gmail.com>
2025-08-03 00:15:49 +0530

    sanitization

--- src/app.js
+++ src/app.js
@@ -21,6 +21,96 @@ app.post("/signup", async (req, res) => {
 }
 });
 
+app.delete("/user", async (req,res)=>{
+    const userId = req.body.userId;
+   try{
+    const user = await User.findByIdAndDelete({_id:userId});
+    if(!user){
+      return res.status(404).send("User not found");
+    }
+    res.send("User deleted successfully");
+  }
+  catch(err){
+   }
+
+})
+
+
+app.get("/feed",async (req,res)=>{
+  try{
+    const user = await User.find({});
+    if(!user){
+      return res.status(404).send("No users found");
+    }    res.send(user);
+  }
+  catch(err){
+    console.error("Error fetching users:", err);
+    res.status(500).send("Error fetching users: " + err.message);
+  }
+
+})
+
+app.patch("/user/:userId",async (req,res)=>{
+  const userId = req.params.userId;
+  const updateData = req.body;
+  try{
+    // Available Feild Updates
+    const AVAILABLE_UPDATES = ["firstName","lastName","age","gender","bio","skills"];
+    const allowedUpdates = Object.keys(updateData).every((key)=>AVAILABLE_UPDATES.includes(key));
+      if(!allowedUpdates){
+        throw new Error("Invalid update fields");
+      }
+
+    // Skills Count Verification
+    const skillscount = updateData.skills.length;
+
+     if(skillscount > 5){
+      throw new Error("You can only add up to 5 skills");
+     }
+     // Password Validation
+     if(updateData.password){
+      const password = updateData.password;
+      const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+      if(!passwordRegex.test(password)){
+        throw new Error("Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character");
+     } 
+     }
+
+     // Age Validation
+     if(updateData.age < 11 || updateData.age > 100){
+      throw new Error("Age must be between 11 and 100");
+     }
+
+    const user = await User.findByIdAndUpdate({_id:userId},updateData);
+    if(!user){   
+      return res.status(404).send("User not found");
+    }
+    res.send("User updated successfully");
+    runValidators:true;
+  }
+  catch(err){
+    res.send("error updating user")
+    console.error("Error updating user:", err);
+}})
+
+app.post("/cast",async (req,res)=>{
+  try{
+    const rawData = {firstName:"John",lastName:"Doe",email:"john@example.com",password:123456,age:"25",gender:"male",bio:"I am a software engineer"};
+  const CastedData =await User.castObject(rawData);
+  const newUser = new User(CastedData);
+  const savedUser = await newUser.save();
+  
+  res.send(CastedData);
+  }
+  catch(err){
+    console.error("Error casting user:", err);
+    res.status(500).send("Error casting user: " + err.message);
+  }
+  
+})
+
+
+
 
 connectdb().then(() => {
   console.log("Database connected successfully");
--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,71 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-   firstName:String,
-   lastName :String,
-    email:String,
-    password:String,
-    age:Number,
-    gender:String,
-    bio:String
-});
+   firstName:{
+    type:String,
+    required:true,
+    minlength:[2,'First name must be at least 3 characters long'],
+    validate(value){
+        const inv = /^[a-zA-Z]+$/;
+        if(!inv.test(value)){
+            throw new Error('First name must contain only letters');
+        }
+    }
+   },
+   lastName :{
+    type:String,
+    required:false,
+    validate(value){
+        const inv = /^[a-zA-Z]+$/;
+        if(!inv.test(value)){
+            throw new Error('Last name must contain only letters');
+        }
+    }
+   },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        match:[/^\S+@\S+\.\S+$/,'Please enter a valid email address']
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:[8,'Password must be at least 8 characters long'],
+        validate(value){
+            const inv = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+            if(!inv.test(value)){
+                throw new Error('Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character');
+            }
+        }
+    },
+    age:{
+        type:Number,
+        required:true,
+        min:[11,'Age must be at least 18'],
+        max:[100,'Age must be less than 100'],
+        required:false
+    },
+    gender:{
+        type:String,
+        validate(value){
+            if(value !== 'male' && value !== 'female' && value!== 'others'){
+                throw new Error('Invalid gender');
+            }
+        },
+        required:false
+    },
+    bio:{
+        type:String,
+        default:"enter about yourself",
+        required:false,
+        maxlength:[500,'Bio must be less than 500 characters']
+    },
+    skills:{
+        type: [String],
+        default:[],
+        required:false
+    }
+   
+},{timestamps:true});
 
 module.exports = mongoose.model('User',userSchema);
